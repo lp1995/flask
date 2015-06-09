c1f9e2843843b4d198055d8ea96ea79bf9914924
liupeng <liupengfun@163.com>
2015-06-09 12:54:55 +0800

    ok

---
+++ run.py
@@ -0,0 +1,30 @@
+from wtforms import Form, BooleanField, TextField, PasswordField, validators
+from flask import Flask ,render_template,request
+app = Flask(__name__)
+
+class RegistrationForm(Form):
+    username = TextField('Username', [validators.Length(min=4, max=25)])
+    email = TextField('Email Address', [validators.Length(min=6, max=35)])
+    password = PasswordField('New Password', [
+        validators.Required(),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
+    confirm = PasswordField('Repeat Password')
+    accept_tos = BooleanField('I accept the TOS', [validators.Required()])
+
+
+@app.route('/', methods=['GET', 'POST'])
+def test():
+    form = RegistrationForm(request.form)
+    if request.method == 'POST' and form.validate():
+        user = User(form.username.data, form.email.data,
+                    form.password.data)
+        db_session.add(user)
+        flash('Thanks for registering')
+        return redirect(url_for('login'))
+    return render_template('index.html', form=form)
+
+
+if __name__  == '__main__':
+    app.debug=True
+    app.run()
